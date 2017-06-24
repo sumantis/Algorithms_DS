2a4457ce5242e464dc2e24908c7b555e4613fa2f
sumanti Saini <sumanti1998@gmail.com>
2017-06-24 19:23:10 +0530

    Selection and bubble repl

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
 	int n;
+	bool sort_further=false;
 	cout<<"number of elements"<<endl;
 	cin>>n;
 	int a[n];
@@ -10,12 +11,17 @@ int main(){
 		cin>>a[i];
 	
 	for(int i=0;i<n;i++)
-		for(int j=0;j<n-i-1;j++){
+		for(int j=0;j<n-1-i;j++){
+			sort_further==false;
 			if(a[j]>a[j+1]){
 				int t=a[j];
 				a[j]=a[j+1];
 				a[j+1]=t;
+				sort_further=true;
 			}
+			if(	sort_further==false)
+				break;
+			
 		}
 		
 	cout<<"sorted"<<endl;
